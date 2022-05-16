0d5438eb08a33baba78356390f6c92fbbafecfa8
juhye87 <78808933+juhye87@users.noreply.github.com>
2022-05-16 12:34:05 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/youthnotification/MainActivity.java
+++ app/src/main/java/com/example/youthnotification/MainActivity.java
@@ -1,14 +1,39 @@
-package com.example.youthnotification;
+package org.techtown.main_page;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
-
+    ActionBar abar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        abar = getSupportActionBar();
+        abar.setDisplayHomeAsUpEnabled(true);
+
+        abar.setLogo(R.drawable.gglogo);
+        abar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MPActivity.class);
+                startActivityForResult(intent,101);
+            } // 인텐트에 메뉴액티비티 넣어서 호출
+        });
     }
 }
