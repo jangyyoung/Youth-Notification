23591fcf94c592d51800e64fc349adce8474341c
juhye87 <78808933+juhye87@users.noreply.github.com>
2022-05-31 19:50:55 +0900

    Update SearchActivity.java
    검색 -> edit 클릭이 안 됨,,

--- app/src/main/java/com/example/youthnotification/SearchActivity.java
+++ app/src/main/java/com/example/youthnotification/SearchActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,17 +21,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
 
+    TextView textView1;
+    TextView textView2;
+    EditText editText;
+
+    String[] items1 = {"전체 선택","취업지원", "창업지원", "주거·금융", "생활·복지", "정책참여", "코로나19"};
+    String[] items2 = {"경기도","수원시", "성남시", "의정부시", "안양시", "부천시", "광명시",
+            "평택시", "동두천시", "안산시", "고양시", "과천시", "구리시", "남양주시", "오산시", "시흥시", "군포시",
+            "의왕시", "하남시", "용인시", "파주시", "이천시", "안성시", "김포시", "화성시", "광주시", "양주시", "포천시", "여주시", "양주군",
+            "여주군", "연천군", "포천군", "가평군", "양평군"};
+
     ActionBar abar;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_1);
 
+        textView1 = findViewById(R.id.in_spinner1);
+        textView2 = findViewById(R.id.in_spinner2);
+        editText = findViewById(R.id.editSearch);
+
+        Spinner spinner1 = findViewById(R.id.first_spinner);
+        Spinner spinner2 = findViewById(R.id.second_spinner);
+
         abar = getSupportActionBar();
         abar.setDisplayHomeAsUpEnabled(true);
 
         abar.setLogo(R.drawable.gglogo);
         abar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, items1);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, items2);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(adapter1);
+        spinner2.setAdapter(adapter2);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView1.setText(items1[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView1.setText("");
+            }
+        });
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView2.setText(items2[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView2.setText("");
+            }
+        });
+
+        Button button = findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = editText.getText().toString();
+                String policy = spinner1.getSelectedItem().toString();
+                String area = spinner2.getSelectedItem().toString();
+
+                Intent intent = new Intent(getApplicationContext(),Search2Activity.class);
+
+                intent.putExtra("query", query);
+                intent.putExtra("policy", policy);
+                intent.putExtra("area", area);
+
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
